b1aef7299ffff026f43c8cc4273a1aa8274a8b2d
Tac Tacelosky <tacman@gmail.com>
2025-04-03 14:56:50 -0400

    add a second record

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,19 +11,40 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $article = new Article();
-        $article->setAuthor('bob');
-        $article->setCurrentLocale('en');
-        $article->setDefaultLocale('en');
-        $article->translate('en')->setTitle('Hello!');
-        $article->translate('fr')->setTitle('Bonjour!');
-        $article->translate('es')->setTitle('Hola!');
-        $manager->persist($article);
+//        $article->setCurrentLocale('en');
+
+        foreach (
+            [
+                     'hello' => [
+                         'es' => 'Hola',
+                         'fr' => 'Bonjour',
+                     ],
+                     'bye' => [
+                         'es' => 'adios',
+                         'fr' => 'au revoir'
+                     ]
+                 ] as $en => $others) {
+            $article = new Article();
+            $article->setAuthor('bob');
+            $article->setDefaultLocale('en');
+
+            $article->translate('en')->setTitle($en);
+            foreach ($others as $locale => $translation) {
+                assert(is_string($translation), json_encode($translation, JSON_PRETTY_PRINT));
+                $article->translate($locale)->setTitle($translation);
+            }
+            $manager->persist($article);
+            $article->mergeNewTranslations();
+        }
+
+//        $article->translate('en')->setTitle('Hello!');
+//        $article->translate('fr')->setTitle('Bonjour!');
+//        $article->translate('es')->setTitle('Hola!');
+//        $manager->persist($article);
         // $product = new Product();
         // $manager->persist($product);
 
 // In order to persist new translations, call mergeNewTranslations method, before flush
-        $article->mergeNewTranslations();
         $manager->flush();
     }
 }

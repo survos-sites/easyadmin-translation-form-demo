5e2367c905fd35ddb789413fb736fc2dda3757d3
Tac Tacelosky <tacman@gmail.com>
2025-04-07 18:27:41 -0400

    add message, get working with tacman bundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,6 +15,7 @@ return [
     A2lix\AutoFormBundle\A2lixAutoFormBundle::class => ['all' => true],
     A2lix\TranslationFormBundle\A2lixTranslationFormBundle::class => ['all' => true],
     Knp\DoctrineBehaviors\DoctrineBehaviorsBundle::class => ['all' => true],
+//    xKnp\DoctrineBehaviors\DoctrineBehaviorsBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
